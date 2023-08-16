28527666acbf76367413e3078002a52970f3af12
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-08-15 20:21:18 -0700

    basic web scraper done, now going to start implementing advanced features and frontend ui

--- app/scraper.py
+++ app/scraper.py
@@ -6,14 +6,24 @@ driver = webdriver.Chrome()
 driver.get('https://nextjs.org/docs')
 
 results = []
+other_results = []
 
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
 
-for element in soup.findAll(attrs={'id': 'what-is-nextjs'}):
-    name = element.find('a') 
+for a in soup.findAll(attrs={'id': 'what-is-nextjs'}):
+    name = a.find('a') 
     if name not in results:
         results.append(name.text)
 
-df = pd.DataFrame({'Names': results})
-df.to_excel('names.xlsx', index=False)
+for b in soup.findAll(attrs={'class': ''}):
+    name2 = b.find('span') 
+    if name2 not in other_results:
+        other_results.append(name2.text)
+
+series1 = pd.Series(results, name='Names')
+series2 = pd.Series(other_results, name= 'Categories')
+df = pd.DataFrame({'Names': series1, 'Categories': series2})
+df.to_csv('names.csv', index=False)
+
+
