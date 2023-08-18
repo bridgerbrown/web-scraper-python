7cb7422a6438f1e79c3551716dbea11111ecd510
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-08-17 21:07:23 -0700

    url and input error handling, submit working, handle functions

--- app/home.tsx
+++ app/home.tsx
@@ -1,23 +1,43 @@
 "use client"
 import Navbar from "@/components/Navbar"
-import React, {useState, ChangeEvent} from "react";
+import React, {useState, ChangeEvent, FormEvent} from "react";
 
 export default function Home() {
   const titleCSS: string = `text-lg font-semibold ml-3`;
   const stepCSS: string = `mb-5 flex justify-center items-center`;
   const stepCircleCSS: string = `w-8 h-8 rounded-full text-white bg-blue-500 flex justify-center items-center`;
   const stepNumberCSS: string = `font-semibold absolute`;
-  const selectDivCSS: string = `mb-16 flex flex-col justify-center items-center`;
   const presetButtonCSS: string = `px-5 py-2 text-sm bg-slate-300 hover:bg-slate-400/60 transition-colors rounded-sm mx-2`;
   const labelCSS: string = `ml-2 mr-6 cursor-pointer`;
 
   const [url, setUrl] = useState<string>("");
+  const [selectedElements, setSelectedElements] = useState<string[]>([]);
+  const [urlError, setUrlError] = useState<boolean>(false);
+  const [elementsError, setElementsError] = useState<boolean>(false);
 
   const handleUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newUrl = event.target.value;
     setUrl(newUrl);
+    setUrlError(false);
     console.log(newUrl);
   };
+
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    if (event.target.checked) {
+      setSelectedElements((prev) => [...prev, value]);
+      console.log(selectedElements)
+      setElementsError(false);
+    } else {
+      setSelectedElements((prev) => prev.filter((element) => element !== value));
+    }
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (!url) setUrlError(true);
+    if (!selectedElements.length) setElementsError(true);
+  }
 
   return (
     <main>
@@ -26,7 +46,7 @@ export default function Home() {
         <h1 className="text-xl mb-12">
           Choose a website to retrieve some HTML elements!
         </h1>
-        <div className={selectDivCSS}>
+        <div className="mb-16 flex flex-col justify-center items-center">
           <span className={stepCSS}>
             <div className={stepCircleCSS}>
               <h2 className={stepNumberCSS}>
@@ -44,7 +64,15 @@ export default function Home() {
             onChange={handleUrlChange}
             className="w-[600px] pl-2 h-8 border-slate-600 border rounded-md"
           />
-          <div className="mt-6 flex flex-col justify-center items-center">
+          {
+            urlError ?
+            <p className="mb-2 pt-3 text-red-600 text-sm">
+              An URL is required!
+            </p>
+            :
+            <div className="mt-10"></div>
+          }
+          <div className="mt-0 flex flex-col justify-center items-center">
             <p className="">
               Or try one of these examples:
             </p>
@@ -70,7 +98,7 @@ export default function Home() {
             </div> 
           </div>
         </div>
-        <div className={selectDivCSS}>
+        <div className="flex flex-col justify-center items-center">
           <span className={stepCSS}>
             <div className={stepCircleCSS}>
               <h2 className={stepNumberCSS}>
@@ -81,7 +109,7 @@ export default function Home() {
               Select which elements to retrieve
             </h2>
           </span>
-          <div className="mt-2 mb-0">
+          <div className="text-center mt-2 mb-0">
             <form 
               id="elements"
               className=""
@@ -90,7 +118,8 @@ export default function Home() {
                 type="checkbox" 
                 id="heading" 
                 value="heading" 
-                name="elements" 
+                name="elements"
+                onChange={handleCheckboxChange}
               />
               <label 
                 htmlFor="heading"
@@ -103,6 +132,7 @@ export default function Home() {
                 id="paragraph" 
                 value="paragraph" 
                 name="elements" 
+                onChange={handleCheckboxChange}
               />
               <label 
                 htmlFor="paragraph"
@@ -115,6 +145,7 @@ export default function Home() {
                 id="links" 
                 value="links" 
                 name="elements" 
+                onChange={handleCheckboxChange}
               />
               <label 
                 htmlFor="links"
@@ -127,6 +158,7 @@ export default function Home() {
                 id="images" 
                 value="images" 
                 name="elements" 
+                onChange={handleCheckboxChange}
               />
               <label 
                 htmlFor="images"
@@ -135,10 +167,21 @@ export default function Home() {
                 Images 
               </label>
             </form>
+            {
+              elementsError ?
+              <p className="mb-7 pt-4 text-red-600 text-sm">
+                Select at least one element to retrieve!
+              </p>
+              :
+              <div className="mb-16"></div>
+            }
           </div>
         </div>
         <div>
-          <button className="px-6 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 drop-shadow-none hover:drop-shadow-lg transition-all">
+          <button 
+            onClick={handleSubmit}
+            className="px-6 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 drop-shadow-none hover:drop-shadow-lg transition-all"
+          >
             Submit
           </button>
         </div>
