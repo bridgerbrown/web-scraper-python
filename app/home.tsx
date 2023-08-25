7f03acf2eff4e923bfc1cc9d4e213354c7fadaf5
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-08-24 21:15:45 -0700

    loading state, browser list step added

--- app/home.tsx
+++ app/home.tsx
@@ -15,11 +15,14 @@ export default function Home() {
 
   const [url, setUrl] = useState<string>("");
   const [selectedElements, setSelectedElements] = useState<string[]>([]);
+  const [browser, setBrowser] = useState<string>("");
   const [urlError, setUrlError] = useState<boolean>(false);
   const [elementsError, setElementsError] = useState<boolean>(false);
+  const [browserError, setBrowserError] = useState<boolean>(false);
   const [status, setStatus] = useState<string>("");
   const [scrapedData, setScrapedData] = useState<any>();
   const [submitted, setSubmitted] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const [headingData, setHeadingData] = useState<any[]>([]);
   const [paragraphData, setParagraphData] = useState<any[]>([]);
@@ -30,27 +33,34 @@ export default function Home() {
     const newUrl = event.target.value;
     setUrl(newUrl);
     setUrlError(false);
-    console.log(newUrl);
   };
 
   const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     if (event.target.checked) {
       setSelectedElements((prev) => [...prev, value]);
-      console.log(selectedElements)
       setElementsError(false);
     } else {
       setSelectedElements((prev) => prev.filter((element) => element !== value));
     }
   };
 
+  const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setBrowser(value);
+    setBrowserError(false);
+  };
+
   const handleSubmit = async (event: FormEvent) => {
+    setLoading(true);
+    setSubmitted(true);
     event.preventDefault();
     if (!url) setUrlError(true);
     if (!selectedElements.length) setElementsError(true);
+    if (!browser) setBrowserError(true);
     if (!url || !selectedElements.length) return;
     await handleScraper();
-    setTimeout(() => console.log(scrapedData), 2000)
+    setLoading(false);
   }
 
   const handleRedo = async (event: FormEvent) => {
@@ -61,6 +71,7 @@ export default function Home() {
     setSelectedElements([]);
     setUrlError(false);
     setElementsError(false);
+    setBrowserError(false);
     setStatus("");
   }
 
@@ -70,19 +81,17 @@ export default function Home() {
         element_types: selectedElements
       };
       const res = await axios.post('http://127.0.0.1:5000/scrape', requestData);
+      setLoading(false);
       setStatus(res.data.error ? res.data.error : res.data.message);
       setScrapedData(res.data.data);
-      console.log(res.data)
       const heading_data = res.data.data[0].length ? [...res.data.data[0].map((item: any) => [item[0], item[1]])] : [];
       const paragraph_data = res.data.data[1].length ? [...res.data.data[1].map((item: any) => item)] : [];
       const link_data = res.data.data[2].length ? [...res.data.data[2].map((item: any) => [item[0], item[1]])] : [];
       const meta_data = res.data.data[3].length ? [...res.data.data[3].map((item: any) => JSON.stringify(item))] : [];
-      console.log(res.data.data)
       setHeadingData(heading_data);
       setParagraphData(paragraph_data);
       setLinkData(link_data);
       setMetaData(meta_data);
-      setSubmitted(true);
     } catch (error) {
       console.log(error);
     }
@@ -90,7 +99,7 @@ export default function Home() {
 
   useEffect(() => {
     console.log(scrapedData) 
-  }, [scrapedData])
+  }, [loading, scrapedData])
 
   return (
     <main>
@@ -99,41 +108,49 @@ export default function Home() {
         {
           submitted
           ?
-          <div className="flex flex-col justify-center items-center">
-            <h1 className="text-3xl font-semibold mt-4 mb-2">
-              Results
-            </h1>
-            {status === "Scraping successful" ? 
-              <h1 className="text-lg text-green-500 mb-6">
-              Scraping successful!
-              </h1>
-              :
-              <h1 className="text-2xl text-red-500 mb-6">
-              Scraping error: {status}
-              </h1>
-            }
-            <section className="w-full text-center flex justify-center items-center mb-4">
-              {
-                scrapedData ? 
-                  <div>
-                    { selectedElements.includes("heading") ? <ResultsList data={headingData} type={"Heading"} /> : <div></div>}
-                    { selectedElements.includes("paragraph") ? <ResultsList data={paragraphData} type={"Paragraph"} /> : <div></div>}
-                    { selectedElements.includes("link") ? <ResultsList data={linkData} type={"Link"} /> : <div></div>}
-                    { selectedElements.includes("meta") ? <ResultsList data={metaData} type={"Meta"} /> : <div></div>}
-                  </div>
-                  :
-                  <p></p>
-              }
-            </section>
-            <div className="w-full flex items-center justify-center">
-              <button 
-                onClick={handleRedo}
-                className="px-6 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 drop-shadow-none hover:drop-shadow-lg transition-all"
-              >
-                Restart
-              </button>
+            loading && !scrapedData.length
+            ?
+            <div className="w-screen flex items-center justify-center">
+              <h2 className="text-2xl">
+                Loading...
+              </h2>
             </div>
-          </div>
+            :
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="text-3xl font-semibold mt-4 mb-2">
+                Results
+              </h1>
+              {status === "Scraping successful" ? 
+                <h1 className="text-lg text-green-500 mb-6">
+                Scraping successful!
+                </h1>
+                :
+                <h1 className="text-2xl text-red-500 mb-6">
+                Scraping error: {status}
+                </h1>
+              }
+              <section className="w-full text-center flex justify-center items-center mb-4">
+                {
+                  scrapedData ? 
+                    <div>
+                      { selectedElements.includes("heading") ? <ResultsList data={headingData} type={"Heading"} /> : <div></div>}
+                      { selectedElements.includes("paragraph") ? <ResultsList data={paragraphData} type={"Paragraph"} /> : <div></div>}
+                      { selectedElements.includes("link") ? <ResultsList data={linkData} type={"Link"} /> : <div></div>}
+                      { selectedElements.includes("meta") ? <ResultsList data={metaData} type={"Meta"} /> : <div></div>}
+                    </div>
+                    :
+                    <p></p>
+                }
+              </section>
+              <div className="w-full flex items-center justify-center">
+                <button 
+                  onClick={handleRedo}
+                  className="px-6 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 drop-shadow-none hover:drop-shadow-lg transition-all"
+                >
+                  Restart
+                </button>
+              </div>
+            </div>
           :
           <div className="flex flex-col justify-center items-center">
             <h1 className="text-3xl font-semibold mb-10">
@@ -191,7 +208,7 @@ export default function Home() {
                 </div> 
               </div>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="mb-2 flex flex-col justify-center items-center">
               <span className={stepCSS}>
                 <div className={stepCircleCSS}>
                   <h2 className={stepNumberCSS}>
@@ -215,7 +232,7 @@ export default function Home() {
                     onChange={handleCheckboxChange}
                   />
                   <label 
-                    htmlFor="headings"
+                    htmlFor="heading"
                     className={labelCSS}
                   >
                     Heading text
@@ -270,7 +287,7 @@ export default function Home() {
                 }
               </div>
             </div>
-            <div className="mb-8 flex flex-col justify-center items-center">
+            <div className="mb-6 flex flex-col justify-center items-center">
               <span className={stepCSS}>
                 <div className={stepCircleCSS}>
                   <h2 className={stepNumberCSS}>
@@ -278,15 +295,89 @@ export default function Home() {
                   </h2>
                 </div>
                 <h2 className={titleCSS}>
-                  Press Submit to start
+                  Select which browser you're using
                 </h2>
               </span>
-              <p className="text-center max-w-xl">
-                The selected website will quickly open and close in a new 
-                  window (don't be alarmed!) for the data to be processed, 
-                  then the results will be displayed here.
-                <br/>
-              </p>
+              <div className="text-center mt-2 mb-0">
+                <form 
+                  id="browsers"
+                  className=""
+                >
+                  <input 
+                    type="radio" 
+                    id="chrome" 
+                    value="chrome" 
+                    name="browsers"
+                    onChange={handleRadioChange}
+                  />
+                  <label 
+                    htmlFor="chrome"
+                    className={labelCSS}
+                  >
+                    Chrome
+                  </label>
+                  <input 
+                    type="radio" 
+                    id="safari" 
+                    value="safari" 
+                    name="browsers" 
+                    onChange={handleRadioChange}
+                  />
+                  <label 
+                    htmlFor="safari"
+                    className={labelCSS}
+                  >
+                    Safari
+                  </label>
+                  <input 
+                    type="radio" 
+                    id="firefox" 
+                    value="firefox" 
+                    name="browsers" 
+                    onChange={handleRadioChange}
+                  />
+                  <label 
+                    htmlFor="firefox"
+                    className={labelCSS}
+                  >
+                    Firefox
+                  </label>
+                  <input 
+                    type="radio" 
+                    id="ie" 
+                    value="ie" 
+                    name="browsers" 
+                    onChange={handleRadioChange}
+                  />
+                  <label 
+                    htmlFor="ie"
+                    className={labelCSS}
+                  >
+                    Internet Explorer
+                  </label>
+                  <input 
+                    type="radio" 
+                    id="other" 
+                    value="other" 
+                    name="browsers" 
+                    onChange={handleRadioChange}
+                  />
+                  <label 
+                    htmlFor="other"
+                    className={labelCSS}
+                  >
+                    Other
+                  </label>
+                </form>
+                {
+                  browserError ?
+                  <p className="mb-5 pt-4 text-red-600 text-sm">
+                    Select a browser!
+                  </p>
+                  :
+                  <div className="mb-14"></div>
+                }
+              </div>
             </div>
             <div className="w-full flex items-center justify-center">
               <button 
