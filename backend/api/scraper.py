adb87e562e7e7185b32c29d7dc97cdf1853d2819
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-01 15:04:46 -0700

    conditional web driver functionality in scraper for different browsers and os's

--- backend/api/scraper.py
+++ backend/api/scraper.py
@@ -1,15 +1,150 @@
+import os
+import platform
 import requests
+import zipfile
+from pathlib import Path
+import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.ie.service import Service as IEService
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import WebDriverException, SessionNotCreatedException
 
 app = Flask(__name__)
 CORS(app)
+
+def download_chromedriver():
+    system_platform = platform.system()
+
+    if system_platform == "Linux":
+        chromedriver_url = f"https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip"
+    elif system_platform == "Darwin":
+        machine = platform.machine()
+        if machine == "arm64":
+            chromedriver_url = f"https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/mac-arm64/chromedriver-mac-arm64.zip"
+        else:
+            chromedriver_url = f"https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/mac-x64/chromedriver-mac-x64.zip"
+    elif system_platform == "Windows":
+        architecture = platform.architecture()[0]
+        if architecture == "32bit":
+            chromedriver_url = f"https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win32/chromedriver-win32.zip"
+        elif architecture == "64bit":
+            chromedriver_url = f"https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win64/chromedriver-win64.zip"
+        else:
+            chromedriver_url = f"https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win64/chromedriver-win64.zip"
+    else:
+        raise Exception(f"Unsupported platform: {system_platform}")
+
+    chromedriver_dir = os.path.join(os.path.dirname(__file__), 'chromedriver')
+    os.makedirs(chromedriver_dir, exist_ok=True)
+
+    chromedriver_path = os.path.join(chromedriver_dir, "chromedriver")
+    if not os.path.exists(chromedriver_path):
+        response = requests.get(chromedriver_url)
+        if response.status_code == 200:
+            with open(os.path.join(chromedriver_dir, "chromedriver.zip"), "wb") as f:
+                f.write(response.content)
+            with zipfile.ZipFile(os.path.join(chromedriver_dir, "chromedriver.zip"), "r") as zip_ref:
+                zip_ref.extractall(chromedriver_dir)
+            os.remove(os.path.join(chromedriver_dir, "chromedriver.zip"))
+            os.chmod(chromedriver_path, 0o755)  # Make the file executable
+        else:
+            raise Exception(f"Failed to download ChromeDriver: {response.status_code}")
+
+    return chromedriver_path
+
+def download_geckodriver():
+    geckodriver_version = "0.33.0"
+    system_platform = platform.system()
+
+    if system_platform == "Linux":
+        geckodriver_url = f"https://github.com/mozilla/geckodriver/releases/download/v{geckodriver_version}/geckodriver-v{geckodriver_version}-linux64.tar.gz"
+    elif system_platform == "Darwin":
+        geckodriver_url = f"https://github.com/mozilla/geckodriver/releases/download/v{geckodriver_version}/geckodriver-v{geckodriver_version}-macos.tar.gz"
+    elif system_platform == "Windows":
+        geckodriver_url = f"https://github.com/mozilla/geckodriver/releases/download/v{geckodriver_version}/geckodriver-v{geckodriver_version}-win64.zip"
+    else:
+        raise Exception(f"Unsupported platform: {system_platform}")
+
+    geckodriver_dir = os.path.join(os.path.dirname(__file__), 'geckodriver')
+    os.makedirs(geckodriver_dir, exist_ok=True)
+
+    geckodriver_path = os.path.join(geckodriver_dir, "geckodriver")
+    if not os.path.exists(geckodriver_path):
+        response = requests.get(geckodriver_url)
+        if response.status_code == 200:
+            with open(os.path.join(geckodriver_dir, "geckodriver.zip"), "wb") as f:
+                f.write(response.content)
+            if system_platform == "Windows":
+                import zipfile
+                with zipfile.ZipFile(os.path.join(geckodriver_dir, "geckodriver.zip"), "r") as zip_ref:
+                    zip_ref.extractall(geckodriver_dir)
+            else:
+                import tarfile
+                with tarfile.open(os.path.join(geckodriver_dir, "geckodriver.tar.gz"), "r:gz") as tar_ref:
+                    tar_ref.extractall(geckodriver_dir)
+            os.remove(os.path.join(geckodriver_dir, "geckodriver.zip"))
+            os.chmod(geckodriver_path, 0o755)
+
+    return geckodriver_path
+
+def download_iedriver():
+    system_architecture = platform.architecture()[0]
+
+    if system_architecture == "32bit":
+        iedriver_url = f"https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.11.0/IEDriverServer_Win32_4.11.0.zip"
+    elif system_architecture == "64bit":
+        iedriver_url = f"https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.11.0/IEDriverServer_x64_4.11.0.zip"
+    else:
+        raise Exception(f"Unsupported system architecture: {system_architecture}")
+
+    iedriver_dir = os.path.join(os.path.dirname(__file__), 'iedriver')
+    os.makedirs(iedriver_dir, exist_ok=True)
+
+    iedriver_path = os.path.join(iedriver_dir, "IEDriverServer.exe")
+    if not os.path.exists(iedriver_path):
+        response = requests.get(iedriver_url)
+        if response.status_code == 200:
+            with open(os.path.join(iedriver_dir, "iedriver.zip"), "wb") as f:
+                f.write(response.content)
+            import zipfile
+            with zipfile.ZipFile(os.path.join(iedriver_dir, "iedriver.zip"), "r") as zip_ref:
+                zip_ref.extractall(iedriver_dir)
+            os.remove(os.path.join(iedriver_dir, "iedriver.zip"))
+
+    return iedriver_path
+
+
+def get_webdriver(browser_type):
+    if browser_type == 'chrome':
+        chromedriver_path = download_chromedriver()
+        service = ChromeService(executable_path=chromedriver_path)
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        return webdriver.Chrome(service=service, options=options)
+    elif browser_type == 'firefox':
+        geckodriver_path = download_geckodriver()
+        service = FirefoxService(executable_path=geckodriver_path)
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        return webdriver.Firefox(service=service, options=options)
+    elif browser_type == 'safari':
+        try:
+            driver = webdriver.Safari()
+        except SessionNotCreatedException:
+            return jsonify({'error': 'Safari WebDriver is not enabled.'})
+    elif browser_type == 'ie':
+        capabilities = DesiredCapabilities.INTERNETEXPLORER.copy()
+        capabilities['ignoreProtectedModeSettings'] = True
+        return webdriver.Ie(capabilities=capabilities)
+    else:
+        raise Exception(f"Unsupported browser type: {browser_type}")
+
 
 @app.route('/scrape', methods=['GET', 'POST'])
 def scrape():
@@ -18,22 +153,8 @@ def scrape():
     elif request.method == 'POST':
         try:
             browser_type = request.json.get('browser')
-
-            if browser_type == 'chrome':
-                options = Options()
-                options.add_argument('--headless')
-                driver = webdriver.Chrome(options=options)
-            elif browser_type == 'firefox':
-                options = FirefoxOptions()
-                options.add_argument('--headless')
-                driver = webdriver.Firefox(options=options)
-            elif browser_type == 'safari':
-                driver = webdriver.Safari()
-            elif browser_type == 'ie':
-                capabilities = DesiredCapabilities.INTERNETEXPLORER.copy()
-                capabilities['ignoreProtectedModeSettings'] = True
-                driver = webdriverIe(capabilities=capabilities)
-
+            driver = get_webdriver(browser_type)
+            
             url = request.json.get('url')
             driver.get(url)
 
