6a8e8a51f301532b1026c4f357ac7d28db1464fc
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-01 16:32:39 -0700

    trying move to inside f

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,20 @@
 import Home from "./home";
 import axios from "axios";
 
-const scraperRequest = async (url: string, selectedElements: any, browser: string) => {
-  "use server"
-  try {
-    const requestData = {
-      url: url,
-      element_types: selectedElements,
-      browser: browser
-    };
-    const res = await axios.post('https://web-scraper-python-backend.vercel.app/scrape', requestData);
-    return res;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 export default async function Page() {
+  const scraperRequest = async (url: string, selectedElements: any, browser: string) => {
+    "use server"
+    try {
+      const requestData = {
+        url: url,
+        element_types: selectedElements,
+        browser: browser
+      };
+      const res = await axios.post('https://web-scraper-python-backend.vercel.app/scrape', requestData);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return <Home scraperRequest={scraperRequest} />
 };
