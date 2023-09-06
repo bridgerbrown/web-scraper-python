6c61678d512060411962f5ce06dfce3a7fcc579a
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-06 15:38:49 -0700

    render-build.sh AND render-start.sh

--- backend/api/render-start.sh
+++ backend/api/render-start.sh
@@ -1,20 +1,3 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-STORAGE_DIR=/opt/render/project/.render
-
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-else
-  echo "...Using Chrome from cache"
-fi
-
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
-python ./backend/api/scraper.py
+python scraper.py
