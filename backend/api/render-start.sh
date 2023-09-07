90485f34069bab9d29c3ab9da332c90a485cc8c0
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-06 20:05:34 -0700

    added echos

--- backend/api/render-start.sh
+++ backend/api/render-start.sh
@@ -1,4 +1,5 @@
+echo "Current working directory after pip install: $(pwd)"
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
-
+echo "Current working directory before running the script: $(pwd)"
 cd ./backend/api
-python scraper.py
+python ./scraper.py
