32e6cf04910af311fd19742ec0bd1d420c9baabe
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-07 14:43:19 -0700

    corrected OS chrome driver links

--- backend/api/render-build.sh
+++ backend/api/render-build.sh
@@ -8,9 +8,46 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
+
+  if [[ "$(uname -s)" == "Linux" ]]; then
+    wget -P ./ https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chrome-linux64.zip
+    dpkg -x ./chrome-linux64.zip $STORAGE_DIR/chrome
+    rm ./chrome-linux64.zip 
+  elif [[ "$(uname -s)" == "Darwin" ]]; then
+    if [[ "$(uname -m)" == "x86_64" ]]; then
+      wget -P ./ https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/mac-x64/chrome-mac-x64.zip
+      hdiutil mount -nobrowse chrome-mac-x64.zip
+      cp -R /Volumes/Google\ Chrome/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $STORAGE_DIR/chrome/
+      hdiutil unmount /Volumes/Google\ Chrome
+      rm chrome-mac-x64.zip 
+    elif [[ "$(uname -m)" == "arm64" ]]; then
+      wget -P ./ https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/mac-arm64/chrome-mac-arm64.zip
+      hdiutil mount -nobrowse chrome-mac-arm64.zip
+      cp -R /Volumes/Google\ Chrome/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $STORAGE_DIR/chrome/
+      hdiutil unmount /Volumes/Google\ Chrome
+      rm chrome-mac-arm64.zip
+    else
+      echo "Unsupported macOS architecture"
+      exit 1
+    fi
+  elif [[ "$(uname -s)" == "Windows" ]]; then
+    if [[ "$(uname -m)" == "x86_64" ]]; then
+      wget -P ./ https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win64/chrome-win64.zip
+      unzip -o ./chrome-win64.zip -d $STORAGE_DIR/chrome/
+      rm ./chrome-win64.zip 
+    elif [[ "$(uname -m)" == "i686" ]]; then
+      wget -P ./ https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win32/chrome-win32.zip
+      unzip -o ./chrome-win32.zip -d $STORAGE_DIR/chrome/
+      rm ./chrome-win32.zip 
+    else
+      echo "Unsupported Windows architecture"
+      exit 1
+    fi
+  else
+    echo "Unsupported operating system"
+    exit 1
+  fi
+
   cd $HOME/backend
 else
   echo "...Using Chrome from cache"
