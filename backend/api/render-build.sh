d690d13b394831b9db1a73362b20305d8cfffb4c
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-15 11:50:27 -0700

    fixed pathing

--- backend/api/render-build.sh
+++ backend/api/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-STORAGE_DIR=/opt/render/web-scraper-python-server/.render
+STORAGE_DIR=/opt/render/project/.render
 
 # if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "Current working directory: $(pwd)"
@@ -15,7 +15,7 @@ STORAGE_DIR=/opt/render/web-scraper-python-server/.render
   ls -l /opt/render/web-scraper-python-server/.render/chrome/opt/google/chrome
   rm ./google-chrome-stable_current_amd64.deb
   echo "Current working directory: $(pwd)"
-  cd $HOME/web-scraper-python-server
+  cd $HOME/project/src/backend
   echo "Current working directory: $(pwd)"
 #else
 #  echo "...Using Chrome from cache"
