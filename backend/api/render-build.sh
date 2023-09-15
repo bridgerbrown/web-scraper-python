050bfed5825099ed33d58f4a0f66bcc92090a7fc
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-15 11:48:13 -0700

    adding more echos

--- backend/api/render-build.sh
+++ backend/api/render-build.sh
@@ -5,8 +5,10 @@ set -o errexit
 STORAGE_DIR=/opt/render/web-scraper-python-server/.render
 
 # if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "Current working directory: $(pwd)"
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
+  echo "Current working directory: $(pwd)"
   cd $STORAGE_DIR/chrome
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
