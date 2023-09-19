fdd22f2bae7e73fe5233f3d0ca91953f5e76fd26
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-09-19 14:41:23 -0700

    turn on debug mode

--- backend/api/scraper.py
+++ backend/api/scraper.py
@@ -94,4 +94,4 @@ def scrape():
             return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
